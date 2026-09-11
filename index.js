33abc42a21d0a4fb56422b138f639aed7d651b0f
Alexander-0-9 <A.kristiansen980@gmail.com>
2026-09-11 13:32:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+// oppgave 1
+console.log("OPPGAVE 1");
+
+const tall = [12, 5, 8, 130, 44, 8, 5, 130];
+
+console.log("1a - Alle elementene");
+
+for (let i = 0; i < tall.length; i++) {
+    console.log(tall[i]);
+}
+
+console.log("1b - Fra siste til første");
+
+for (let i = tall.length - 1; i >= 0; i--) {
+    console.log(tall[i]);
+}
+
+console.log("1c - Indeksert utskrift");
+
+for (let i = 0; i < tall.length; i++) {
+    console.log(i + ":" + tall[i]);
+}
+
+console.log("1d - Sum og gjennomsnitt");
+
+let sum = 0;
+
+for (let i = 0; i < tall.length; i++) {
+    sum += tall[i];
+}
+
+let gjennomsnitt = sum / tall.length;
+
+console.log("Sum:", sum);
+console.log("Gjennomsnitt:", gjennomsnitt);
+
+
+// oppgave 2
+console.log("OPPGAVE 2");
+
+function minVerdi(arr) {
+    let min = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+
+function maxVerdi(arr) {
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+console.log("2a - Min og maks");
+console.log("Min:", minVerdi(tall));
+console.log("Maks:", maxVerdi(tall));
+
+function kvadrater(arr) {
+    let nyArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        nyArray.push(arr[i] * arr[i]);
+    }
+
+    return nyArray;
+}
+
+console.log("2b - Kvadrater");
+console.log(kvadrater(tall));
+
+function barePartall(arr) {
+    let nyArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            nyArray.push(arr[i]);
+        }
+    }
+
+    return nyArray;
+}
+
+console.log("2c - Partall");
+console.log(barePartall(tall));
+
+function utenDuplikater(arr) {
+    let nyArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let finnes = false;
+
+        for (let j = 0; j < nyArray.length; j++) {
+            if (arr[i] === nyArray[j]) {
+                finnes = true;
+                break;
+            }
+        }
+
+        if (!finnes) {
+            nyArray.push(arr[i]);
+        }
+    }
+
+    return nyArray;
+}
+
+console.log("2d - Uten duplikater");
+console.log(utenDuplikater(tall));
